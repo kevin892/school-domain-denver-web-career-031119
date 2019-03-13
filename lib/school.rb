fc3e377b1652f4533c38091c535c5d1202ee0cd6
github username <github email address>
2019-03-13 12:57:58 -0600

    Add school class. All tests pass.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+require 'pry'
+class School
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = Hash.new
+  end
+
+  def add_student(student, grade)
+    roster[grade] ||= []
+    roster[grade] << student
+  end
+
+def grade(i)
+  roster.map do |k, v|
+    return v if k == i
+  end
+end
+
+def sort
+  sort_hash = Hash.new
+  roster.each do |k, v|
+    sort_hash[k] = v.sort
+  end
+  sort_hash
+end
+
+end
